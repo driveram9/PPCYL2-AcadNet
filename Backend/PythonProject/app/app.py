5ecd6584ee5c36cedb11a47925f3d69d4dda88ec
pablo <jposadass6@miumg.edu.gt>
2026-04-03 22:17:48 -0600

    .

--- Backend/PythonProject/app/app.py
+++ Backend/PythonProject/app/app.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
-@app.route('/')
+@app.route("/")
 def index():
-    cursos= ['kdslfj', 'fañskdfj', 'flkañsdfj' ,'Programacion' ]
-    data={
-        'Titulo':'Backend',
-        'Bienvenida': 'Saludos',
-        'cursos': cursos,
-        'numero_cursos': len(cursos)
+    return render_template("index.html")
 
-    }
-    return render_template("index.html", data = data)
+@app.route("/login", methods=["POST"])
+def login():
+    usuario = request.form['usuario']
+    contrasenia = request.form['contrasenia']
+
+
+    if usuario == "AdminPPCYL2" and contrasenia == "AdminPPCYL2771":
+        return redirect(url_for("admin_dashboard"))
+    else:
+        return ("Lo sentimos, no fue posible encontrar el usuario ingresado")
+
+@app.route("/admin")
+def admin_dashboard():
+    return render_template("admin.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
-

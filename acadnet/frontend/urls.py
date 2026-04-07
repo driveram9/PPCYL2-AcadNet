9368811f381a4187b5702883641e9ae4de10f880
fernandogonzalezt2 <fgonzalezt2@miumg.edu.gt>
2026-04-07 17:43:40 -0600

    Tabla

--- acadnet/frontend/urls.py
+++ acadnet/frontend/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('upload/', views.upload_view, name='upload'),
+    path('tabla/', views.tabla_view, name='tabla'),
 ]

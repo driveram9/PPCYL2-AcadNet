e7ded9952f4f347ba0c2baae3872a252b91cf842
ipereza6-dotcom <ipereza6@miumg.edu.gt>
2026-04-09 18:30:38 -0600

    Update dashboardStudent.js

--- acadnet/frontend/static/js/dashboardStudent.js
+++ acadnet/frontend/static/js/dashboardStudent.js
@@ -10,3 +10,25 @@ allSideMenu.forEach(item=> {
         li.classList.add('active');
     })
 });
+const menuItems = document.querySelectorAll('#sidebar .side-menu.top li a');
+const sections = document.querySelectorAll('main .section');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+        // Ocultar todas las secciones
+        sections.forEach(sec => sec.style.display = 'none');
+
+        // Quitar la clase active de todos
+        menuItems.forEach(i => i.parentElement.classList.remove('active'));
+
+        // Activar el clic actual
+        this.parentElement.classList.add('active');
+
+        // Mostrar la sección correspondiente
+        const texto = this.querySelector('.text').textContent.toLowerCase();
+        const target = document.getElementById(texto);
+        if (target) target.style.display = 'block';
+    });
+});
+

4d6ea3c9f4571a2c83dbe2d2556a61fe7b37af24
Alejandro <jcaballerost@miumg.edu.gt>
2026-04-11 21:27:21 -0600

    actualizacion

--- acadnet/frontend/urls.py
+++ acadnet/frontend/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('student/', views.dashboard_student, name='student'),
     path('admin/', views.dashboard_admin, name='admin'),
     path('tutor/', views.dashboard_tutor, name='dashboard_tutor'),
-    path('upload/', views.upload_view, name='upload'),
-    path('tabla/', views.tabla_view, name='tabla'),
+   # path('upload/', views.upload_view, name='upload'),
+    #path('tabla/', views.tabla_view, name='tabla'),
 ]

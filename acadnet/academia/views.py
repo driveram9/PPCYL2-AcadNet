0f136272f585167d91aecc665966b444a3b5c01b
ipereza6-dotcom <ipereza6@miumg.edu.gt>
2026-04-13 01:20:18 -0600

    Update views.py

--- acadnet/academia/views.py
+++ acadnet/academia/views.py
@@ -2,30 +2,30 @@ from django.shortcuts import render, redirect
 import xml.etree.ElementTree as ET
 import os
 
-RUTA_XML = os.path.join(os.path.dirname(__file__),'registro.xml')  # Se asigna una variable para asignar la ruta de base de la datos
+RUTA_XML = os.path.join(os.path.dirname(__file__), 'registro.xml')
 
 
-def cargar_usuarios():  # se crea una funcion para asignar los datos del XML
-    usuarios = {"estudiantes": {}, "tutores": {}}  # Se cargan los estudiantes y tutores
+def cargar_usuarios():
+    usuarios = {"estudiantes": {}, "tutores": {}}
 
-    if not os.path.exists(RUTA_XML):  # Si el archivo de la base de datos no existe o se encuentra en otra ruta
-        print(f" No se encuentra {RUTA_XML}")  # imprime la ubicacion actual del archivo
+    if not os.path.exists(RUTA_XML):
+        print(f"No se encuentra {RUTA_XML}")
         return usuarios
 
-    tree = ET.parse(RUTA_XML)  # Funciones propias de la libreria elementTree
-    root = tree.getroot()  # Funciones propias de la libreria elementTree
+    tree = ET.parse(RUTA_XML)
+    root = tree.getroot()
 
-    # Cargar estudiantes (usuario = carnet)
+    # Cargar estudiantes
     for est in root.findall(".//estudiante"):
-        carnet = est.get("carnet")  # Se usa el Numero de carnet como usuario para iniciar secion
+        carnet = est.get("carnet")
         usuarios["estudiantes"][carnet] = {
             "nombre": est.text,
             "contrasenia": est.get("contrasenia")
         }
 
-        # Cargar tutores (usuario = registro_personal)
+    # Cargar tutores
     for tut in root.findall(".//tutor"):
-        registro = tut.get("registro_personal")  # Se usa el registro_personal como usuario para iniciar secion
+        registro = tut.get("registro_personal")
         usuarios["tutores"][registro] = {
             "nombre": tut.text,
             "contrasenia": tut.get("contrasenia")
@@ -35,44 +35,38 @@ def cargar_usuarios():  # se crea una funcion para asignar los datos del XML
 
 def login_view(request):
     if request.method == 'POST':
-        usuario = request.POST.get('usuario')  # Obtiene el usuario colocado para el inicio de secion
-        contrasenia = request.POST.get('contrasenia')  # Obtiene la contraseña colocado para el inicio de secion
+        usuario = request.POST.get('usuario')
+        contrasenia = request.POST.get('contrasenia')
 
-        usuarios = cargar_usuarios()  # Se llama a la funsion de cargar usuario para para validar el rol del
-        # ingreso que se esta ingresando, se asigna a una variable para que este pueda ser validadacon los if
+        usuarios = cargar_usuarios()
 
-        # Condicional si el usuario llega a hacer el Admin
-        if usuario == "AdminPPCYL2" and contrasenia == "AdminPPCYL2771":  # Credenciales dadas por defecto segun el cliente
-            request.session['rol'] = 'admin'  # Se asigna el rol de Admin
+        # Admin
+        if usuario == "AdminPPCYL2" and contrasenia == "AdminPPCYL2771":
+            request.session['rol'] = 'admin'
             request.session['nombre'] = 'Administrador del Sistema'
             request.session['usuario'] = usuario
-            return redirect('admin')
+            return redirect('admin_dashboard')
 
-        # Condicional si el usuario llega a ser un estudiante
-        if usuario in usuarios[
-            "estudiantes"]:  # Se verifica si el usuario asignado pertenece a estudiantes, de no ser asi pasa al siguiente if de tutores
-            if contrasenia == usuarios["estudiantes"][usuario][
-                "contrasenia"]:  # Despues de validar usuario valida la contraseña
-                request.session['rol'] = 'estudiante'  # Se le agrega un rol de estudiante
+        # Estudiante
+        if usuario in usuarios["estudiantes"]:
+            if contrasenia == usuarios["estudiantes"][usuario]["contrasenia"]:
+                request.session['rol'] = 'estudiante'
                 request.session['nombre'] = usuarios["estudiantes"][usuario]["nombre"]
                 request.session['carnet'] = usuario
                 return redirect('student')
 
-        # Condicional si el usuario llega a ser un tutor
-        if usuario in usuarios[
-            "tutores"]:  # Se verifica si el usuario asignado pertenece a tutores, de no ser asi pasa al siguiente if de usuario erroneo
-            if contrasenia == usuarios["tutores"][usuario][
-                "contrasenia"]:  # Despues de validar usuario valida la contraseña
-                request.session['rol'] = 'tutor'  # Se le agrega un rol de tutor
+        # Tutor
+        if usuario in usuarios["tutores"]:
+            if contrasenia == usuarios["tutores"][usuario]["contrasenia"]:
+                request.session['rol'] = 'tutor'
                 request.session['nombre'] = usuarios["tutores"][usuario]["nombre"]
                 request.session['registro'] = usuario
                 return redirect('tutor')
 
-    return redirect('login')
+    return render(request, "login.html")
 
 
-def dashboard_student(request):
-    """Panel del Estudiante"""
+def estudiante_dashboard(request):
     if request.session.get('rol') != 'estudiante':
         return redirect('login')
     return render(request, 'dashboardStudent.html', {
@@ -81,8 +75,7 @@ def dashboard_student(request):
     })
 
 
-def dashboard_admin(request):
-    """Panel del Administrador"""
+def admin_dashboard(request):
     if request.session.get('rol') != 'admin':
         return redirect('login')
     return render(request, 'dashboardAdmin.html', {
@@ -91,8 +84,7 @@ def dashboard_admin(request):
     })
 
 
-def dashboard_tutor(request):
-    """Panel del Tutor"""
+def tutor_dashboard(request):
     if request.session.get('rol') != 'tutor':
         return redirect('login')
     return render(request, 'dashboardTutor.html', {

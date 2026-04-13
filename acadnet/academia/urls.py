4bdb073ea916cd896b3be58b5494eb85b1c5f020
ipereza6-dotcom <ipereza6@miumg.edu.gt>
2026-04-13 00:36:03 -0600

    Update urls.py

--- acadnet/academia/urls.py
+++ acadnet/academia/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('student/', views.estudiante_dashboard, name='student'),
     path('admin/', views.admin_dashboard, name='admin'),
     path('tutor/', views.tutor_dashboard, name='tutor'),
-    #path('upload/', views.upload_view, name='upload'),
-    #path('tabla/', views.tabla_view, name='tabla'),
 ]
